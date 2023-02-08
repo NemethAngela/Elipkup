49bc0c23d8078b668cd3b6fec49de06e66bc2a96
NemethAngela <nemeth@gmail.com>
2023-02-08 17:55:35 +0100

    Kezdes

--- src/app.js
+++ src/app.js
@@ -3,25 +3,30 @@
 * Author: Németh Angéla
 * Copyright: 2023, Németh Angéla
 * Group: Szoft 1-2 E
-* Date: 2023-02-04
+* Date: 2023-02-07
 * Github: https://github.com/NemethAngela/Elipkup
 * Licenc: GNU GPL
 */
 
-const r1radiusElem = document.querySelector("#r1radius");
-const r2radiusElem = document.querySelector("#r2radius");
-const heightElem = document.querySelector("#height");
+const diameterElem = document.querySelector("#diameter");
 const calcButton = document.querySelector("#calcButton");
-const eredmenyElem = document.querySelector("#eredmeny");
+const terfogatElem = document.querySelector("#terfogat");
+const felszinElem = document.querySelector("#felszin");
 
 calcButton.addEventListener('click', () => {
-    let r1radius = Number(r1radiusElem.value);
-    let r2radius = Number(r2radiusElem.value);
-    let height = Number(heightElem.value);
-    let eredmeny = calcVolume(r1radius, r2radius, height);
-    eredmenyElem.value = eredmeny.toFixed(3);  
+    var diameter = Number(diameterElem.value);
+    var terfogat = calcVolume(diameter);
+    var felszin = calcSurface(diameter);
+    terfogatElem.value = terfogat.toFixed(3);  
+    felszinElem.value = felszin.toFixed(3);
 });
 
-function calcVolume(r1, r2, h) {
-    return (Math.PI / 3) * r1 * r2 * h;
+function calcVolume(d) {
+    let r = d / 2;
+    return (((4 * Math.PI) * (r * r * r)) / 3);
+}
+
+function calcSurface(d) {
+    let r = d / 2;
+    return ((4 * Math.PI) * r * r);
 }
